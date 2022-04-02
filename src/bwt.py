57c1a41c0d19cf60d0c4522266c34cde00123b34
Thomas Mailund <mailund@birc.au.dk>
2022-04-02 10:12:36 +0200

    Removed solution

--- src/bwt.py
+++ src/bwt.py
@@ -1,10 +1,5 @@
 """Burrows-Wheeler transform."""
 
-from alphabet import Alphabet
-from sa import (
-    buckets,
-    prefix_doubling,
-)
 
 # I would normally use '\x00' for the sentinel, but doctest doesn't
 # like that (because it is gods damned stupid). For this example,
@@ -19,41 +14,7 @@ def bwt(x: str) -> str:
     >>> bwt('mississippi')
     'ipssm$pissii'
     """
-    sa = prefix_doubling(x)
-    return ''.join([x[i - 1] if i > 0 else '$' for i in sa])
-
-
-def ctable(z: list[int]) -> list[int]:
-    """
-    Compute the C table for a mapped string.
-
-    >>> ctable([1, 2, 1, 4, 5, 0])
-    [0, 1, 3, 4, 4, 5]
-    """
-    # The C table is really just the buckets from a bucket sort.
-    return buckets(z)
-
-
-def otable(z: list[int]) -> list[list[int]]:
-    """
-    Compute the O table for a mapped string.
-
-    >>> otab = otable([1, 2, 0, 2, 3])
-    >>> for a, row in enumerate(otab):
-    ...    print(f"{a}: {row}")
-    0: [0, 0, 0, 1, 1, 1]
-    1: [0, 1, 1, 1, 1, 1]
-    2: [0, 0, 1, 1, 2, 2]
-    3: [0, 0, 0, 0, 0, 1]
-    """
-    sigma = max(z) + 1
-    otab = [
-        [0] * (len(z) + 1) for _ in range(sigma)
-    ]
-    for a in range(sigma):
-        for i, b in enumerate(z):
-            otab[a][i + 1] = otab[a][i] + (a == b)
-    return otab
+    return 'FIXME'
 
 
 def rbwt(y: str) -> str:
@@ -63,12 +24,4 @@ def rbwt(y: str) -> str:
     >>> rbwt('ipssm$pissii')
     'mississippi'
     """
-    alpha = Alphabet(y)
-    z = alpha.map(y)
-    ctab = ctable(z)
-    otab = otable(z)
-    i, x = 0, [0] * (len(y) - 1)
-    for j in reversed(range(len(z) - 1)):
-        a = x[j] = z[i]
-        i = ctab[a] + otab[a][i]
-    return alpha.revmap(x)
+    return 'FIXME'
